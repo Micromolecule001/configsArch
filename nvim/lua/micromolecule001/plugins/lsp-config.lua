1ddc046f729bb90e31f4270a15c8416e475762cd
Micromolecule001 <dima.boyko.bd@gmail.com>
2024-09-13 15:46:34 +0300

    lsp fixes

--- nvim/lua/micromolecule001/plugins/lsp-config.lua
+++ nvim/lua/micromolecule001/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "html_ls" }
+                ensure_installed = { "lua_ls", "ts_ls", "html", "cssls" }
             })
         end,
     },
@@ -22,7 +22,8 @@ return {
 
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
-            lspconfig.html_ls.setup({})
+            lspconfig.html.setup({})
+            lspconfig.cssls.setup({})
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
